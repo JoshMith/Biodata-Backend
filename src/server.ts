6d0ac928e7558bdbfcb03d1d0c74e48666e98ff8
JoshMith <150962666+JoshMith@users.noreply.github.com>
2025-05-19 18:44:51 +0300

    dfbs

--- src/server.ts
+++ src/server.ts
@@ -34,7 +34,7 @@ app.use(express.urlencoded({ extended: true })) // for parsing application/x-www
 app.use(cookieParser())
 
 
-const allowedOrigins = ['http://localhost:5173', 'http://localhost:4200', 'https://christian-biodata-git-main-joshmiths-projects.vercel.app/', 'https://christian-biodata-frontend.vercel.app'];
+const allowedOrigins = ['http://localhost:5173', 'http://localhost:4200', 'https://christian-biodata-git-main-joshmiths-projects.vercel.app', 'https://christian-biodata-frontend.vercel.app'];
 
 //CORS middleware
 app.use(cors({
